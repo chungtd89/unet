815bb8fddeef7899ded18a5ddbad0d4923edea70
chungtd89 <49780689+chungtd89@users.noreply.github.com>
2019-06-01 14:08:29 +0700

    trainGenerator target size from 256,256 to 640,480

--- data.py
+++ data.py
@@ -47,7 +47,7 @@ def adjustData(img,mask,flag_multi_class,num_class):
 
 def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
                     mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
-                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (640,480),seed = 1):
     '''
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
